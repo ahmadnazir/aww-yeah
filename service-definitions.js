56a341f98b34761fe93cccb332048cc08b96e947
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-06-15 13:49:13 +0200

    fix git ignore and definitions file

--- service-definitions.js
+++ service-definitions.js
@@ -39,7 +39,13 @@ var services = {
         }
     },
     frontend: {
-        name: "fe-application-loader"
+        name: "fe-application-loader",
+        commands: {
+            up: 'npm start #aww-frontend',
+            start: 'npm start #aww-frontend',
+            stop: "ps ax | grep \\#aww-frontend | grep -v grep | awk '{print $1}' | xargs -I % kill %",
+            isRunning: "ps ax | grep \\#aww-frontend | grep -v grep"
+        }
     },
     form: {
         name: "forms",
